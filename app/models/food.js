42569c6cd2c72c0e30dd8e7f9c85df3e478ce42f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-01 17:11:02 -0600

    Food model validation

--- app/models/food.js
+++ app/models/food.js
@@ -1,4 +1,40 @@
+const rules = {
+  name:     { required: true },
+  calories: { required: true }
+};
+
 class Food {
+  constructor(opts) {
+    let keys = Object.keys(opts);
+    for(let key of keys) {
+      this[key] = opts[key];
+    }
+
+    this._valid = true;
+    this._validate();
+  }
+
+  _validate() {
+    let keys = Object.keys(rules);
+    for(let key of keys) {
+      if(!this._checkValid(key)) {
+        this._valid = false;
+        return;
+      }
+    }
+  }
+
+  _checkValid(key) {
+    if(!rules[key]) return true;
+    if(rules[key].required === true && !this[key]) return false;
+
+    return true;
+  }
+
+  get isValid() {
+    return this._valid;
+  }
+
 
 }
 
