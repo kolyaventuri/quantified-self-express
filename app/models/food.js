73cd5265957ae2fd38cf36d6b41be82f9a927438
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-01 16:54:20 -0600

    Dynamic model loading in tests

---
+++ app/models/food.js
@@ -0,0 +1,5 @@
+class Food {
+
+}
+
+module.exports = Food;
--- spec/helpers/test_setup.js
+++ spec/helpers/test_setup.js
@@ -1,4 +1,13 @@
 const DatabaseCleaner = require('./databaseCleaner');
+const glob = require('glob');
+const path = require('path');
+
+// Load models
+let files = glob.sync(path.join(__dirname, '../../app/models/**/*.js'));
+let models = files.map(require);
+for(let model of models) {
+  global[model.name] = model;
+}
 
 beforeEach((done) => {
   DatabaseCleaner.clean()
