a1cbc5dbf29a8d1e601f91a8f2f6e5743d82034d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 12:52:18 -0600

    Meal should have many foods spec

--- spec/models/meal_spec.js
+++ spec/models/meal_spec.js
@@ -6,4 +6,28 @@ describe('Meal', () => {
     expect(meal.isValid).to.be.false;
     expect(meal2.isValid).to.be.true;
   });
+
+  it('should have many foods', (done) => {
+    let meal = new Meal({ name: 'Breakfast' });
+    let foods = new Array(3).map(() => {
+      new Food({ name: 'Toast', calories: 30 }).save()
+    });
+
+    Promise.all(foods)
+      .then((data) => {
+        expect(meal.foods).to.be.an('array');
+        expect(meal.foods).to.have.lengthOf(0);
+
+        meal.foods.push(foods[0]);
+        meal.foods.push(foods[1]);
+        meal.save().then(data => {
+          expect(data.foods).to.have.lengthOf(2);
+          done();
+        }).catch(err => {
+          done(err);
+        })
+      }).catch(err => {
+        done(err);
+      });
+  });
 });
