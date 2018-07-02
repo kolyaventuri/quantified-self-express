2029ff5be3cb0031eb51a143f7a4729d2f70c7af
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 15:07:45 -0600

    Re-add missing code for meal food show back into controller (whoops)

--- app/controllers/meals/foodsController.js
+++ app/controllers/meals/foodsController.js
@@ -23,6 +23,26 @@ class FoodsController {
       res.status(500).send();
     });
   }
+
+  static show(req, res, next) {
+    Meal.find(req.params.meal_id).then(meal => {
+
+      meal.foods
+        .then(foods => {
+          let _foods = foods.map(food => food.serialized);
+          meal = Object.assign(meal.serialized, { foods: _foods });
+
+          res.json(meal);
+        }).catch(err => {
+          console.error(err);
+          res.status(500).send();
+        });
+
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send();
+    });
+  }
 }
 
 module.exports = FoodsController;
