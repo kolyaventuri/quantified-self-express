b052ddfb1b0028ea68b056b96eae65a812131eda
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 11:15:39 -0600

    Pull foods route handling into controller

---
+++ app/controllers/foodsController.js
@@ -0,0 +1,36 @@
+const Food = require('../models/food');
+
+class FoodsController {
+  static index(req, res, next) {
+    Food.all().then(foods => {
+      res.json(foods.map(food => food.serialized));
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send();
+    });
+  }
+
+  static show(req, res, next) {
+    Food.find(req.params.id).then(food => {
+      res.json(food.serialized);
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send();
+    });
+  }
+
+  static create(req, res, next) {
+    let food = new Food(req.body.food);
+
+    if(!food.isValid) return res.status(400).send();
+
+    food.save().then(data => {
+      res.json(data[0]);
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send();
+    });
+  }
+}
+
+module.exports = FoodsController;
--- routes/api/v1/foods/index.js
+++ routes/api/v1/foods/index.js
@@ -1,37 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const Food = require('../../../../app/models/food');
+const FoodsController = require('../../../../app/controllers/foodsController');
 
-router.get('/', (req, res, next) => {
-  Food.all().then(foods => {
-    res.json(foods.map(food => food.serialized));
-  }).catch(err => {
-    console.error(err);
-    res.status(500).send();
-  });
-});
+router.get('/', FoodsController.index);
 
-router.get('/:id', (req, res, next) => {
-  Food.find(req.params.id).then(food => {
-    res.json(food.serialized);
-  }).catch(err => {
-    console.error(err);
-    res.status(500).send();
-  });
-});
+router.get('/:id', FoodsController.show);
 
-router.post('/', (req, res, next) => {
-  let food = new Food(req.body.food);
-
-  if(!food.isValid) return res.status(400).send();
-
-  food.save().then(data => {
-    res.json(data[0]);
-  }).catch(err => {
-    console.error(err);
-    res.status(500).send();
-  });
-});
+router.post('/', FoodsController.create);
 
 module.exports = router;
