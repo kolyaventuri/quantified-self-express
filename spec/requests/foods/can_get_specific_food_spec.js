8be8d7d166397ce69d55c1c9a9bb2fc1acdd58ca
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 10:38:04 -0600

    Fix test of get all foods to handle situation where promises dont come back in order

---
+++ spec/requests/foods/can_get_specific_food_spec.js
@@ -0,0 +1,37 @@
+describe('A GET request to a specific /api/v1/foods/:id', () => {
+  it('should return a JSON object representing the food', (done) => {
+    let foods = [
+      { name: 'Toast', calories: 10 },
+      { name: 'Toast', calories: 20 },
+      { name: 'Toast', calories: 30 },
+    ];
+
+    Promise.all([
+      new Food(foods[0]).save(),
+      new Food(foods[1]).save(),
+      new Food(foods[2]).save(),
+    ]).then((data) => {
+      chai.request(app)
+        .get(`/api/v1/foods/${data[0][1].id}`)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+
+          let food = data[0][1];
+
+          expect(res.body).to.have.property('id').that.eqls(food.id);
+          expect(res.body).to.have.property('name').that.eqls(food.name);
+          expect(res.body).to.have.property('calories').that.eqls(food.calories);
+
+          expect(res.body).to.not.have.property('created_at');
+          expect(res.body).to.not.have.property('updated_at');
+
+          done();
+        });
+    }).catch(err => {
+      done(err);
+    });
+
+  });
+});
--- spec/requests/foods/can_list_foods_spec.js
+++ spec/requests/foods/can_list_foods_spec.js
@@ -10,7 +10,13 @@ describe('A GET request to /api/v1/foods', () => {
       new Food(foods[0]).save(),
       new Food(foods[1]).save(),
       new Food(foods[2]).save(),
-    ]).then(() => {
+    ]).then((data) => {
+      data = data.sort((a, b) => {
+        a = a[0]; b = b[0];
+        if(a.id > b.id) return 1;
+        if(a.id < b.id) return -1;
+        return 0;
+      });
       chai.request(app)
         .get('/api/v1/foods')
         .end((err, res) => {
@@ -19,9 +25,9 @@ describe('A GET request to /api/v1/foods', () => {
           expect(res.body).to.be.an('array');
           expect(res.body).to.have.lengthOf(3);
 
-          for(let i = 0; i < foods.length; i++) {
-            let food = foods[i];
-            expect(res.body[i].id).to.eq(i + 1);
+          for(let i = 0; i < data.length; i++) {
+            let food = data[i][0];
+            expect(res.body[i].id).to.eq(food.id);
             expect(res.body[i].name).to.eq(food.name);
             expect(res.body[i].calories).to.eq(food.calories);
             expect(res.body[i]).to.not.have.property('created_at');
