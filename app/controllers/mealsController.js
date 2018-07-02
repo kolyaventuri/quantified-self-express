17be9d89cb06802fa6ffe66ab97e3572ef0ea237
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 13:59:09 -0600

    Meals index controller

--- app/controllers/mealsController.js
+++ app/controllers/mealsController.js
@@ -2,7 +2,16 @@ const Meal = require('../models/meal');
 
 class MealsController {
   static index(req, res, next) {
-    
+    Meal.all().then(meals => {
+      meals = meals.map(meal => {
+        return meal.serialized;
+      });
+
+      res.json(meals);
+    }).catch(err => {
+      console.error();
+      res.status(500).send();
+    });
   }
 }
 
