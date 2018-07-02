c76eb089f335a541cda4c7ef3d5da8921f649dbe
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-02 14:38:03 -0600

    Get singular meal with foods

--- routes/api/v1/meals/index.js
+++ routes/api/v1/meals/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const MealsController = require('../../../../app/controllers/mealsController');
+const FoodsController = require('../../../../app/controllers/meals/foodsController');
 
 router.get('/', MealsController.index);
+router.get('/:meal_id/foods', FoodsController.show);
 
 module.exports = router;
