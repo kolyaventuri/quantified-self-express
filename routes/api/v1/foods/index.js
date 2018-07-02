39c7427d209d7c2e3321d199e219da477f83d985
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-01 18:04:34 -0600

    Add sad path test and write food create handler

--- routes/api/v1/foods/index.js
+++ routes/api/v1/foods/index.js
@@ -4,8 +4,16 @@ var router = express.Router();
 const Food = require('../../../../app/models/food');
 
 router.post('/', (req, res, next) => {
-  console.log(req.body);
-  res.json(req.body);
+  let food = new Food(req.body.food);
+
+  if(!food.isValid) return res.status(400).send();
+
+  food.save().then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.error(err);
+    res.status(500).send();
+  })
 });
 
 module.exports = router;
--- spec/requests/foods/can_create_food_spec.js
+++ spec/requests/foods/can_create_food_spec.js
@@ -1,16 +1,32 @@
-describe('A POST request to /api/v1/foods', (done) => {
-  let food = {
-    name: 'Test',
-    calories: 20
-  };
-  chai.request(app)
-    .post('/api/v1/foods')
-    .send(food)
-    .end((err, res) => {
-      expect(res).to.have.status(200);
-      expect(res.body).to.be.an('object');
-      expect(res.body).to.have.property('id');
-      expect(res.body).to.have.property('name').that.eqls(food.name);
-      expect(res.body).to.have.property('calories').that.eqls(food.calories);
-    });
+describe('A POST request to /api/v1/foods', () => {
+  it('should create a food', (done) => {
+    let food = {
+      name: 'Test',
+      calories: 20
+    };
+    chai.request(app)
+      .post('/api/v1/foods')
+      .send(food)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('id');
+        expect(res.body).to.have.property('name').that.eqls(food.name);
+        expect(res.body).to.have.property('calories').that.eqls(food.calories);
+        done();
+      });
+  });
+
+  it('should return a 400 status if the food is invalid', (done) => {
+    let food = {
+      name: 'Test'
+    };
+    chai.request(app)
+      .post('/api/v1/foods')
+      .send(food)
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        done();
+      });
+  });
 });
