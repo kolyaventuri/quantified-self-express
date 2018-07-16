0aa2a437cc68f2930496fe51e148cde845fefc25
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-16 15:11:52 -0600

    Add test for proper pagination

--- spec/requests/foods/can_get_recipes_for_food_spec.js
+++ spec/requests/foods/can_get_recipes_for_food_spec.js
@@ -1,5 +1,7 @@
 describe('A GET request to a specific /api/v1/foods/:id/recipes', () => {
-  it('returns a JSON object of up to 10 recipes', (done) => {
+  let recipeOne = null;
+
+  it('returns a JSON object of 3 recipes', (done) => {
     let food = new Food({ name: 'Banana', calories: 80 })
     food.save().then(() => {
       Food.all().then(foods => {
@@ -20,6 +22,37 @@ describe('A GET request to a specific /api/v1/foods/:id/recipes', () => {
 
             expect(recipes[0]).to.have.property('name');
             expect(recipes[0]).to.have.property('url');
+
+            recipeOne = recipes[0];
+            done();
+          });
+      });
+    });
+  });
+
+  it('can return the next 3 items', (done) => {
+    let food = new Food({ name: 'Banana', calories: 80 })
+    food.save().then(() => {
+      Food.all().then(foods => {
+        food = foods[0];
+
+        chai.request(app)
+          .get(`/api/v1/foods/${food._data.id}/recipes?page=2`)
+          .end((err, res) => {
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+
+            let result = res.body;
+
+            let recipes = result.recipes;
+
+            expect(recipes).to.be.an('array');
+
+            expect(recipes[0]).to.have.property('name');
+            expect(recipes[0]).to.have.property('url');
+
+            expect(recipes[0].name).to.not.eql(recipeOne.name);
             done();
           });
       });
