cd130cb8f1b683e371d8d2335ba65f210290fd13
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-16 12:38:05 -0600

    Add request builder

--- app/services/requestBuilder.js
+++ app/services/requestBuilder.js
@@ -1,5 +1,27 @@
+const querystring = require('querystring');
+
 class RequestBuilder {
-  
+
+  constructor(url, headers) {
+    this.BASE_URL = url;
+    this.headers = headers;
+  }
+
+  build(url, params) {
+    let _url = `${this.BASE_URL}${url}`;
+    let query = querystring.stringify(params);
+
+    let opts = {
+      uri: `${_url}?${query}`,
+      headers: this.headers
+    };
+
+    return opts;
+  }
+
+  static register(url, headers) {
+    return new this(url, headers);
+  }
 }
 
 module.exports = RequestBuilder;
