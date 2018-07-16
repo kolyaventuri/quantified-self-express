d6166a75241ac14f313b4ab33670b47f50eae78e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-16 12:25:08 -0600

    Build Yumly service

--- app/services/yummlyService.js
+++ app/services/yummlyService.js
@@ -1,5 +1,11 @@
+const APIService = require('./APIService');
+
 class Yumly {
-  
+
+  static async recipes(query) {
+    return await APIService.get('/recipe', { q: query })
+  }
+
 }
 
 module.exports = Yumly;
