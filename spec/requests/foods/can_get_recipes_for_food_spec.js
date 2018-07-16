42a0f24273ca63f2f183b50dc27bda24f1db52d8
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-16 13:16:44 -0600

    Recipes endpoint test

--- spec/requests/foods/can_get_recipes_for_food_spec.js
+++ spec/requests/foods/can_get_recipes_for_food_spec.js
@@ -1,5 +1,26 @@
-xdescribe('A GET request to a specific /api/v1/foods/:id/recipes', () => {
-  it('returns a JSON object of up to 10 recipes', () => {
+describe('A GET request to a specific /api/v1/foods/:id/recipes', () => {
+  it('returns a JSON object of up to 10 recipes', async () => {
+    let food = new Food({ name: 'Banana', calories: 80 })
+    await food.save();
 
+    let foods = await Food.all();
+    food = foods[0];
+
+    chai.request(app)
+      .get(`/api/v1/foods/${food._data.id}/recipes`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+
+        let result = res.body;
+
+        let recipes = result.recipes;
+
+        expect(recipes).to.be.an('array');
+
+        expect(recipes[0]).to.have.property('name');
+        expect(recipes[0]).to.have.property('url');
+      });
   });
 });
